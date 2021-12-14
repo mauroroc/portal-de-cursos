942b3e3581d539f94e34cb026acb37a830f4cfea
Mauro Da Cunha Rocha <mauro.rocha@al.infnet.edu.br>
2021-12-13 21:19:41 -0300

    Aula 01

---
+++ README.md
@@ -0,0 +1,3 @@
+Projeto Portal de Cursos
+
+Iniciando o create-react-app e limpando o que não precisa
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Button } from 'react-bootstrap'
+
+function App() {
+  return (
+    <div>
+      <p>Portal de Cursos</p>
+      <button className="btn btn-primary">Teste</button>
+      <Button variant='danger'>Teste</Button>
+    </div>
+  );
+}
+
+export default App;
