f61b8d115211aa6c60d6c93ec9587abc6c95eeae
Mauro Da Cunha Rocha <mauro.rocha@al.infnet.edu.br>
2021-12-15 20:15:14 -0300

    header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import { Button } from 'react-bootstrap'
+import { Navbar, Container, Nav } from 'react-bootstrap'
+import Logo from './assets/img/logo-infnet.png'
 
 function App() {
   return (
-    <div>
-      <p>Portal de Cursos</p>
-      <button className="btn btn-primary">Teste</button>
-      <Button variant='danger'>Teste</Button>
-    </div>
+    <header>
+      <Navbar expand="md" className="main-navbar">
+        <Container>
+          <Navbar.Brand href="/">
+            <img src={Logo} alt="Logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/cursos">Cursos</Nav.Link>          
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
   );
 }
 
