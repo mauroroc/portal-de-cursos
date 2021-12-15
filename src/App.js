90de8a2e47ad256bb93bddf444f4f6e94b119524
Mauro Da Cunha Rocha <mauro.rocha@al.infnet.edu.br>
2021-12-15 20:36:41 -0300

    banner da home

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import { Navbar, Container, Nav } from 'react-bootstrap'
+import React from 'react'
+import { Button, Navbar, Container, Nav } from 'react-bootstrap'
 import Logo from './assets/img/logo-infnet.png'
+import Banner from './assets/img/banner.jpg'
 
 function App() {
   return (
-    <header>
-      <Navbar expand="md" className="main-navbar">
+    <>
+      <header>
+        <Navbar expand="md" className="main-navbar">
+          <Container>
+            <Navbar.Brand href="/">
+              <img src={Logo} alt="Logo" />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ms-auto">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/cursos">Cursos</Nav.Link>          
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </header>
+      <main>
         <Container>
-          <Navbar.Brand href="/">
-            <img src={Logo} alt="Logo" />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/cursos">Cursos</Nav.Link>          
-            </Nav>
-          </Navbar.Collapse>
+          <div className='shadow border my-3 p-4 banner-home'>
+            <div>
+              <h1>Bem vindo ao Infnet!</h1>
+              <p>Conheça nossos curso de tecnologia</p>
+              <p>Faça sua inscrição agora mesmo</p>
+              <Button href="'/cursos" className="mb-2">CURSOS</Button>
+            </div>
+            <div>
+              <img src={Banner} alt="banner" className="img-fluid" />
+            </div>          
+          </div>
         </Container>
-      </Navbar>
-    </header>
+        
+      </main>
+    </>
   );
 }
 
