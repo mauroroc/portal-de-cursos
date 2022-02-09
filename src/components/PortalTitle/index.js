a966e7e56931c7968c7a77fb26497e04365225c0
Mauro Rocha <mauro.rocha@al.infnet.edu.br>
2022-02-09 20:31:22 -0300

    Tela de Cadastro de Curso

--- src/components/PortalTitle/index.js
+++ src/components/PortalTitle/index.js
@@ -1,5 +1,14 @@
-export function PortalTitle ({ children }) {
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+export function PortalTitle ({ children, buttonText, buttonLink }) {
   return (
-    <h1 className="mt-4">{children}</h1>
+    <div className="mt-4 d-sm-flex align-items-center">
+      <h1 className="mb-0">{children}</h1>
+      {buttonText && buttonLink && (
+        <Button className="ms-sm-3"size="sm" as={Link} to={buttonLink}>{buttonText}</Button>
+      )}
+      
+    </div>    
   )
 }
